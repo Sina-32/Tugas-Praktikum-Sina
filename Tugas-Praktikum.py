c35e282cdd0d13afaccf9d636ace48a515ce3dac
Sina-32 <357.skbm@gmail.com>
2024-11-07 09:21:24 +0700

    Mengedit Login Status

--- Tugas-Praktikum.py
+++ Tugas-Praktikum.py
@@ -14,8 +14,16 @@ def login():
             return
         else:
             kesempatan -= 1
-            print(f"Username atau password salah! Kesempatan tersisa: {kesempatan}")
+            if kesempatan == 0:
+                break  # Keluar dari loop langsung jika kesempatan habis
+            if username != username_terdaftar and password != password_terdaftar:
+                print(f"Username dan password salah! Kesempatan tersisa: {kesempatan}")
+            elif username != username_terdaftar:
+                print(f"Username salah! Kesempatan tersisa: {kesempatan}")
+            elif password != password_terdaftar:
+                print(f"Password salah! Kesempatan tersisa: {kesempatan}")
     
-    print("Anda telah keluar dari halaman login")
+    print("Anda telah keluar dari sistem login")
 
 login()
+
