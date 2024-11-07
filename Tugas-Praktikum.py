2c4d333a88b814107147403f649c24ee55b8aa36
Sina-32 <357.skbm@gmail.com>
2024-11-07 10:55:50 +0700

    Mengedit Login Pesan Status

--- Tugas-Praktikum.py
+++ Tugas-Praktikum.py
@@ -3,7 +3,7 @@ print(f"Silahkan Login")
 def login():
     username_terdaftar = "Daspro2024"
     password_terdaftar = "Latihan"
-    kesempatan = 3
+    kesempatan = 5
     
     while kesempatan > 0:
         username = input("Masukkan username: ")
@@ -15,13 +15,13 @@ def login():
         else:
             kesempatan -= 1
             if kesempatan == 0:
-                break  # Keluar dari loop langsung jika kesempatan habis
+                break  
             if username != username_terdaftar and password != password_terdaftar:
-                print(f"Username dan password salah! Kesempatan tersisa: {kesempatan}")
+                print(f"Username dan password anda salah kesempatan tersisa: {kesempatan}")
             elif username != username_terdaftar:
-                print(f"Username salah! Kesempatan tersisa: {kesempatan}")
+                print(f"Username anda salah kesempatan tersisa: {kesempatan}")
             elif password != password_terdaftar:
-                print(f"Password salah! Kesempatan tersisa: {kesempatan}")
+                print(f"Password anda salah kesempatan tersisa: {kesempatan}")
     
     print("Anda telah keluar dari sistem login")
 
